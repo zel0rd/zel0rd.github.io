d82bf4ce0107f5a03e2404dcdc8c42b002fabede
younggil <zelord.kwoun@gmail.com>
2021-11-09 03:30:11 +0900

    [ADD] github.io index page, with todo-list, vending-machine, json-parser

--- src/javascripts/setEventEditCard.js
+++
@@ -1,9 +0,0 @@
-let editEvent = () => {
-  let cards = document.querySelectorAll(".todo-list > li");
-  // cards[0].children[0].children[1];
-  cards.forEach((card) => {
-    card.children[1].addEventListener("dblcick", function () {
-      card[1].classList.toggle("editing");
-    });
-  });
-};
