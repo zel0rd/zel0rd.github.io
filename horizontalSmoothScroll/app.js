f8445ec6797f82685b2405b3e8460bad8486a0eb
younggil <zelord.kwoun@gmail.com>
2021-11-26 11:18:13 +0900

    [ADD] Horizontal Smooth Scrolling Parallax Slider

---
+++ horizontalSmoothScroll/app.js
@@ -0,0 +1,52 @@
+console.log("TEST")
+
+let images = [...document.querySelectorAll('.img')];
+let slider = document.querySelector('.slider');
+let sliderWidth;
+let imageWidth;
+let current = 0;
+let target = 0;
+let ease = 0.05;
+
+window.addEventListener('resize', init)
+
+images.forEach((img, idx) => {
+    img.style.backgroundImage = `url(./images/${idx + 1}.jpg)`
+})
+
+function lerp(start, end, t) {
+    return start * (1 - t) + end * t;
+}
+
+function setTransform(el, transform) {
+    el.style.transform = transform;
+}
+
+function init() {
+    sliderWidth = slider.getBoundingClientRect().width;
+    imageWidth = sliderWidth / images.length;
+    document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
+}
+
+function animate() {
+    current = parseFloat(lerp(current, target, ease)).toFixed(2);
+    target = window.scrollY;
+    setTransform(slider, `translateX(-${current}px)`)
+    animateImages()
+    requestAnimationFrame(animate)
+}
+
+function animateImages() {
+    let ratio = current / imageWidth;
+    let intersectionRatioValue;
+
+    images.forEach((image, idx) => {
+        intersectionRatioValue = ratio - (idx * 0.7);
+        setTransform(image, `translateX(${intersectionRatioValue * 70}px)`)
+    })
+}
+
+init();
+animate();
+
+console.log(imageWidth)
